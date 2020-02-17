6f04fd986b59b4230ea54200773f4723bcb9fb0d
Arthur Deruytter <arthur.deruytter@ugent.be>
2020-02-17 20:08:08 +0100

    Het werkt nu op de manier dat ze wouden

--- app/src/main/java/com/example/oef/MainActivity.kt
+++ app/src/main/java/com/example/oef/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.oef
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
 
 class MainActivity : AppCompatActivity() {
@@ -47,12 +51,45 @@ class MainActivity : AppCompatActivity() {
 
 }
 
-class ForecastOverviewFragment : AppCompatActivity() {
+class ForecastOverviewFragment : Fragment() {
 
+    var TAG = "FORECAST"
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_forecast_overview, container, false)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_forecast_overview)
+        Log.v(TAG, " CREATE")
+    }
 
+    override fun onStart() {
+        super.onStart()
+        Log.v(TAG, " START")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.v(TAG, " RESUME")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.v(TAG, " PAUSE")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.v(TAG, " STOP")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v(TAG, " DESTROY")
     }
 
 }
