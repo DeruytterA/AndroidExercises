19162e0997394f3cad21d92962506948f540f85d
Arthur Deruytter <arthur.deruytter@ugent.be>
2020-02-19 23:01:35 +0100

    added gitignore and added onclicklistner to the refresh button

--- app/src/main/java/com/example/oef/MainActivity.kt
+++ app/src/main/java/com/example/oef/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.ObservableInt
 import androidx.fragment.app.Fragment
 import com.example.oef.databinding.FragmentForecastOverviewBinding
 
@@ -53,12 +54,14 @@ class MainActivity : AppCompatActivity() {
 }
 
 data class Temp(
-    val temp: Int
+    var temp: ObservableInt
 )
 
 class ForecastOverviewFragment : Fragment() {
 
     var TAG = "FORECAST"
+
+    lateinit var temperature: Temp
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -66,16 +69,18 @@ class ForecastOverviewFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentForecastOverviewBinding.inflate(inflater, container, false)
-        binding.temp = Temp(14)
+        binding.temp = temperature
+        binding.button.setOnClickListener{
+            temperature.temp.set(temperature.temp.get() + 1)
+        }
         return  binding.root
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        temperature = Temp(ObservableInt(16))
         Log.v(TAG, " CREATE")
     }
-
-
 
     override fun onStart() {
         super.onStart()
